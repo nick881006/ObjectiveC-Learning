4fe6a28daf44b1507668742b33af9b418952e238
nick lee <nicklee1006@gmail.com>
2017-11-25 23:13:45 +1030

    Initial Commit

---
+++ ObjectiveC-Learning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC-Learning
+//
+//  Created by Nick on 2017/11/25.
+//  Copyright © 2017年 Nick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
